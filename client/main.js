c2075ea869d81dd4ff4adfd810ca9352c0c800fe
SaschaG <sushi.genji@hotmail.de>
2017-01-22 21:38:36 +0100

    Build in basic switcg of the content when the radio buttons for lebenslauf or portfolio are clicked. Its a dirty way to do over display:none. In future this should be updated using reactivity

--- client/main.js
+++ client/main.js
@@ -1,22 +1,30 @@
 import { Template } from 'meteor/templating';
 import { ReactiveVar } from 'meteor/reactive-var';
+import { Session } from 'meteor/session';
 
 import './main.html';
 
-Template.hello.onCreated(function helloOnCreated() {
-  // counter starts at 0
-  this.counter = new ReactiveVar(0);
+
+
+Template.content.onCreated(function helloOnCreated() {
+  
 });
 
-Template.hello.helpers({
-  counter() {
-    return Template.instance().counter.get();
-  },
+Template.content.helpers({
+
 });
 
-Template.hello.events({
-  'click button'(event, instance) {
-    // increment the counter when button is clicked
-    instance.counter.set(instance.counter.get() + 1);
+Template.content.events({
+  'click input'(event, template) {
+
+if(template.$('#lebenslauf').is(":checked")){
+	$("#content-lebenslauf").css("display", "block");
+	$("#content-portfolio").css("display", "none");
+}
+else{
+	$("#content-lebenslauf").css("display", "none");
+	$("#content-portfolio").css("display", "block");
+}
+	
   },
 });
